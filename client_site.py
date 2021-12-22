7de224883dd8df4e3fd830103faed9c3b1099345
Dominik Gawlak <dominikpriv6@gmail.com>
2021-12-22 19:58:10 +0100

    connected host with server, added game template.

--- client_site.py
+++ client_site.py
@@ -8,16 +8,36 @@ class Test:
         self.context = zmq.Context()
         print("connecting to sever")
         self.socket = self.context.socket(zmq.REQ)
-        self.socket.connect("tcp://localhost:5555")
+        self.socket.connect("tcp://localhost:"+str(self.init_game()))
 
     def connect(self):
-        for request in range(10):
-            print(f"sending request {request}")
-            self.socket.send(b"Hello")
+        while True:
+            self.socket.send(b"connected")
 
             self.message = self.socket.recv()
-            print(f"Recieved reply {request}, {self.message}")
+            #print(f"{self.message}")
+            if self.message == b'connected':
+                print("connected to host")
+                self.game()
+            else:
+                print("not connected")
 
+            time.sleep(1)
+
+
+
+        #for request in range(10):
+            #print(f"sending request {request}")
+            #self.socket.send(b"Hello")
+
+
+
+    def init_game(self):
+        id = input("Type id to connect with host: ")
+        return id
+    
+    def game(self):
+        pass
 
 test = Test()
 test.connect()
--- server_site.py
+++ server_site.py
@@ -18,21 +18,63 @@ class Server:
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)
         self.socket.bind("tcp://*:5555")
-        #self.connect()
         self.packet_listener()
 
-    def connect(self):
-
-
-        print("enable listener..")
-
-        
     def packet_listener(self):
+        print("enable listener.. \n\n")
+
+        while True:
+            message = self.socket.recv()
+            print(message)
+            if message == b'connected':
+                print("Connected. \n")
+                self.socket.send(b"connected")
+                self.game()
+            else:
+                self.socket.send(b"not connected")
+                print("not connected")
+
+            time.sleep(1)
+
+    def init_game(self):
+        id = 5555
+        print('''Hello in this super extra cool game \n
+                You are the host now, give this number to your opponent to connect \n
+                 ''' + f'ID: {id}'
+        )
+        print('''
+                 Menu: \n
+                 1. Type: 'start' to start the game \n
+                 2. 'stop' to stop the game \n
+        ''')
+    
+    def generate_word(self):
+        word = input("Type word to guess: ")
+        return word
+    
+    def game(self):
+        self.init_game()
+
+        command = input()
+
+        if command.lower() == "start":
+            self.word = self.generate_word()
+            self.socket.send(b"Your opponent set word to guess. Now is your turn to answer. Type your word")
+
         while True:
             self.message = self.socket.recv()
-            print(f"Received request: {self.message}")
-            time.sleep(1)
-            self.socket.send(b"World")
+            #print(f"Received request: {self.message}")
+            
+            print(f"Your opponent said: {self.message} \n")
+
+            if self.message.lower() == self.word.lower():
+                print("Your opponent guessed your secret word, you lose..")
+                break
+            else:
+                print("Your opponent did not guess, type: \n 1. 'hint' to send him hint \n 2.'again' to give him another chance \n ")
+            
+            time.sleep(0.5)
+            #self.socket.send(b"World")
 
 
 
